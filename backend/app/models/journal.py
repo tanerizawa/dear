fb0ed1cd8eb8f8903d06f2030735471320417e5c
tanerizawa <studiomalaka@gmail.com>
2025-06-22 07:56:21 +0700

    Add User import to journal model

--- backend/app/models/journal.py
+++ backend/app/models/journal.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from app.db.base_class import Base
+from app.models.user import User
 import datetime
 
 class Journal(Base):
