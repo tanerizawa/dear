e1204868a44a19b51a4f68bda91dc681db089f45
tanerizawa <studiomalaka@gmail.com>
2025-06-23 14:55:07 +0700

    Add current user endpoint

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1 import auth, journal, chat
+from app.api.v1 import auth, journal, chat, user
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(journal.router, prefix="/journals", tags=["journals"])
 api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+api_router.include_router(user.router, tags=["users"])
 
