f87790847565041b41f4a58de27939af45483daa
tanerizawa <studiomalaka@gmail.com>
2025-06-24 09:02:10 +0700

    Add context-based UiText string resolver

--- app/src/main/java/com/psy/dear/core/UiText.kt
+++ app/src/main/java/com/psy/dear/core/UiText.kt
@@ -1,5 +1,6 @@
 package com.psy.dear.core
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
@@ -7,6 +8,11 @@ import androidx.compose.ui.res.stringResource
 sealed class UiText {
     data class DynamicString(val value: String) : UiText()
     data class StringResource(@StringRes val resId: Int) : UiText()
+}
+
+fun UiText.asString(context: Context): String = when (this) {
+    is UiText.DynamicString -> value
+    is UiText.StringResource -> context.getString(resId)
 }
 
 @Composable
