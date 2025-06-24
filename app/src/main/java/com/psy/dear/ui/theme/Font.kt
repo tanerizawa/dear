994b592227ccfa7d0256b33a01a9dcd2df905fbc
tanerizawa <studiomalaka@gmail.com>
2025-06-24 11:32:58 +0700

    Add custom fonts and typography

---
+++ app/src/main/java/com/psy/dear/ui/theme/Font.kt
@@ -0,0 +1,16 @@
+package com.psy.dear.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.psy.dear.R
+
+val Lora = FontFamily(
+    Font(R.font.lora_regular, FontWeight.Normal),
+    Font(R.font.lora_bold, FontWeight.Bold)
+)
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_bold, FontWeight.Bold)
+)
