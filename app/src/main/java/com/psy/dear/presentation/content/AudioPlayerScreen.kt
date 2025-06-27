804fd75426a6e5cb2ce35ad971a502f58a850ca6
tanerizawa <studiomalaka@gmail.com>
2025-06-27 07:21:04 +0000

    Remove unused LocalContext reference in audio player

--- app/src/main/java/com/psy/dear/presentation/content/AudioPlayerScreen.kt
+++ app/src/main/java/com/psy/dear/presentation/content/AudioPlayerScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,7 +30,6 @@ fun AudioPlayerScreen(
     trackUrl: String,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
-    val context = LocalContext.current
     val state by homeViewModel.state.collectAsState()
     val tracks = state.audio + state.recommendedTracks
     var currentIndex by remember(tracks, trackUrl) {
